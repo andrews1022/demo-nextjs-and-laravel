d341ae25186657bcae0f3b351fb9a7dc7d63a5ea
Andrew Shearer <andrewshearerdev@gmail.com>
2026-02-27 13:12:54 -0800

    added /hello route to api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\MessageController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// This will be accessible at /api/hello
+Route::get('/hello', [MessageController::class, 'getHello']);
