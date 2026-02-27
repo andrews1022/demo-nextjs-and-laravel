c0b715c4059488ae28eeb91f61c8410bc7ca4b33
Andrew Shearer <andrewshearerdev@gmail.com>
2026-02-27 11:58:45 -0800

    edited / cleaned up next.js project files

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false, // hide next-js-toast indicator
   reactCompiler: true,
 };
 
