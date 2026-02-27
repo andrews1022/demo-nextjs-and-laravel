834cd8498df03d153a60b918a22c60096648af48
Andrew Shearer <andrewshearerdev@gmail.com>
2026-02-27 13:11:58 -0800

    configured MessageController to send back dummy json response

--- laravel/app/Http/Controllers/MessageController.php
+++ laravel/app/Http/Controllers/MessageController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function getHello(): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Hello World from Laravel!',
+            'status' => 'success',
+            'data' => [
+                'version' => '1.0.0',
+                'author' => 'Andrew'
+            ]
+        ]);
+    }
 }
