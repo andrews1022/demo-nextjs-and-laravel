d887ad896511a5a70509d6e94f4c1d6dfec44d6f
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 10:47:28 -0800

    updated JsonUserSeeder to read data from local json file

--- laravel/database/seeders/JsonUserSeeder.php
+++ laravel/database/seeders/JsonUserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
 
 class JsonUserSeeder extends Seeder
 {
@@ -12,6 +14,28 @@ class JsonUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Locate the local JSON file
+        $path = database_path('data/users.json');
+
+        // Read the file
+        $json = File::get($path);
+
+        // Convert JSON string to a PHP array
+        $users = json_decode($json, true);
+
+        // Loop through and save to the database
+        foreach ($users as $userData) {
+            User::updateOrCreate(
+                ['id' => $userData['id']], // Use the ID from JSON as the unique check
+                [
+                    'name'     => $userData['name'],
+                    'email'    => $userData['email'],
+                    'username' => $userData['username'],
+                    'phone'    => $userData['phone'],
+                    'website'  => $userData['website'],
+                    'password' => Hash::make('password'), // Required by the users table
+                ]
+            );
+        }
     }
 }
