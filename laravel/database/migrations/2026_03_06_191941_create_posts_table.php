60302597211b9360c5890aadadd02f13d73095a7
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 11:23:26 -0800

    updated up() method in create posts table migration file

--- laravel/database/migrations/2026_03_06_191941_create_posts_table.php
+++ laravel/database/migrations/2026_03_06_191941_create_posts_table.php
@@ -12,8 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // This maps to "id" in the local JSON file
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // This creates the link to the users table
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps(); // Adds nullable creation and update timestamps to the table
         });
     }
 
