ba1e01a6cd411c1ff3588b2f518352d055337eb2
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 10:48:14 -0800

    updated User model fillable array

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'id',
         'name',
         'email',
         'password',
+        'username',
+        'phone',
+        'website'
     ];
 
     /**
