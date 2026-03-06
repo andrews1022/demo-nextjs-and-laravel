27898261ca9156ef9a285bd5b0e3b7b4bf8ee441
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 14:04:29 -0800

    updated comment model

--- laravel/app/Models/Comment.php
+++ laravel/app/Models/Comment.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Comment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'id',
+        'post_id',
+        'name',
+        'email',
+        'body',
+    ];
+
+    /**
+     * Define the relationship to the Post model
+     *
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
