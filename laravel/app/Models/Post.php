582f1919cd30708c49f369c440783fe266bf38c6
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 11:28:16 -0800

    added fillable array and user relationship definition method to post model

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'id',
+        'user_id',
+        'title',
+        'body'
+    ];
+
+    /**
+     * Define the relationship to the User model
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
