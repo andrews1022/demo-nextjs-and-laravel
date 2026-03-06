a1852d7143db1b66df11f081c0dbde5ac5c847c4
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 11:34:03 -0800

    updated JsonPostSeeder run() method

--- laravel/database/seeders/JsonPostSeeder.php
+++ laravel/database/seeders/JsonPostSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class JsonPostSeeder extends Seeder
 {
@@ -12,6 +13,25 @@ class JsonPostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Locate the local JSON file
+        $path = database_path('data/posts.json');
+
+        // Read the file
+        $json = File::get($path);
+
+        // Convert JSON string to a PHP array
+        $posts = json_decode($json, true);
+
+        // Loop through and save to the database
+        foreach ($posts as $postData) {
+            Post::updateOrCreate(
+                ['id' => $postData['id']],
+                [
+                    'user_id' => $postData['userId'], // Mapping JSON userId to DB user_id
+                    'title'   => $postData['title'],
+                    'body'    => $postData['body'],
+                ]
+            );
+        }
     }
 }
