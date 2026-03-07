ebcb8c6ad93d1452325f70d03be23c51faa3d2b0
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-06 18:03:15 -0800

    updated CommentFactory definition() method

--- laravel/database/factories/CommentFactory.php
+++ laravel/database/factories/CommentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'name'    => fake()->name(),
+            'email'   => fake()->safeEmail(),
+            'body'    => fake()->paragraph(),
         ];
     }
 }
