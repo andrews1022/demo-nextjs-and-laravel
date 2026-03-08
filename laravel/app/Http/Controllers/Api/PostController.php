7e39c2fe14e57c8328df1466f79a4ebb8e377625
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-08 11:07:00 -0700

    updated PostController index() method

--- laravel/app/Http/Controllers/Api/PostController.php
+++ laravel/app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        // 'with' is Eager Loading - it prevents the N+1 query problem
+        return Post::with('user')->latest()->get();
+    }
 }
