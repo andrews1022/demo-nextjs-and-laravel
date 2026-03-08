7abb86aa5cc18e593fd4fa16b23f7320eb44c81d
Andrew Shearer <andrewshearerdev@gmail.com>
2026-03-08 11:07:16 -0700

    simplified api.php with only post controller and route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\MessageController;
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// This will be accessible at /api/hello
-Route::get('/hello', [MessageController::class, 'getHello']);
+// This will be accessible at /api/posts
+Route::get('/posts', [PostController::class, 'index']);
